4d5987444206931a759feb3bd5dfa203d179f3c9
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-10-09 00:30:32 +0200

    create api

---
+++ contact2/app/Providers/AppServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Cache;
+use Auth0\Login\LaravelCacheWrapper;
+use Auth0\Login\Contract\Auth0UserRepository as Auth0UserRepositoryContract; 
+use Auth0\Login\Repository\Auth0UserRepository as Auth0UserRepository;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Set 10 seconds leeway to avoid Exception 'Cannot handle token prior to [timestamp]'
+        // If you still get this Exception don't set a higher leeway,
+        // consider synchronize your server with NTP instead.
+        // For more information see: https://github.com/auth0/auth0-PHP/issues/56
+        \Firebase\JWT\JWT::$leeway = 10;
+
+        $this->app->bind(
+            Auth0UserRepositoryContract::class,
+            Auth0UserRepository::class
+        );
+        
+        // This is used for RS256 tokens to avoid fetching the JWKs on each request
+        $this->app->bind(
+            '\Auth0\SDK\Helpers\Cache\CacheHandler',
+            function() {
+                static $cacheWrapper = null;
+                if ($cacheWrapper === null) {
+                    $cache = Cache::store();
+                    $cacheWrapper = new LaravelCacheWrapper($cache);
+                }
+                return $cacheWrapper;
+            });
+    }
+}
