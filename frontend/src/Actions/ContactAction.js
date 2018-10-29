eac09e7dbb4f97f53812cf428a4bb6ec85348452
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-10-30 00:43:57 +0100

    add new functions

--- frontend/src/Actions/ContactAction.js
+++ frontend/src/Actions/ContactAction.js
@@ -3,9 +3,36 @@ export const addContact = contact => ({
   payload: contact,
 });
 
+export const editContact = (id, contact) => ({
+  type: 'EDIT_CONTACT',
+  payload: id,
+  value: contact,
+});
+
 export const deleteContact = id => ({
   type: 'DELETE_CONTACT',
   payload: id,
+});
+
+export const selectContact = id => ({
+  type: 'SELECT_CONTACT',
+  payload: id
+});
+
+export const unSelectContact = () => ({
+  type: 'UNSELECT_CONTACT',
+})
+
+export const sortContactsByName = () => ({
+  type: 'SORT_CONTACTS_BY_NAME',
+});
+
+export const listAllContacts = () => ({
+  type: 'LIST_ALL_CONTACTS',
+});
+
+export const listAllNameOfContacts = () => ({
+  type: 'LIST_ALL_NAME_CONTACTS',
 });
 
 export const updateForm = contact => ({
--- frontend/src/Reducers/ContactReducer.js
+++ frontend/src/Reducers/ContactReducer.js
@@ -7,11 +7,9 @@ const initialState = {
     },
   ],
   form: {
-    create: {
-      sub: '',
-      name: '',
-      phone: '',
-    }
+    sub: '',
+    name: '',
+    phone: '',
   },
 }
 
@@ -23,10 +21,47 @@ export default (state = initialState, action) => {
         ...state,
         contacts: [...state.contacts, action.payload]
       };
-      
-    case 'DELETE_NOTE':
+
+    case 'EDIT_CONTACT':
+      return {
+        contacts: [...state.contacts.map((c => {
+          if (c.id === action.payload) c = action.value
+          return c
+        }))],
+      };
+
+    case 'DELETE_CONTACT':
+      fetch('http://localhost:3010/api/contacts/' + action.payload, { method: "DELETE" })
+        .catch(console.log);
       return {
         contacts: state.contacts.filter((c => c.id !== action.payload))
+      };
+
+    // case 'SELECT_CONTACT':
+    //   return {
+    //     contacts: [...state.contacts],
+    //     selectedContact: state.contacts.filter(c => c.id === action.payload)[0]
+    //   };
+
+    case 'UNSELECT_CONTACT':
+      return {
+        contacts: [...state.contacts],
+      };
+
+    case 'SORT_CONTACT_BY_NAME':
+      let contacts = [...state.contacts]
+      return {
+        contacts: contacts.sort((a, b) => (a.name.toLocaleLowerCase() > b.name.toLocaleLowerCase()) ? 1 : 0),
+      };
+
+    case 'LIST_ALL_CONTACTS':
+      return {
+        contacts: [...state.contacts],
+      };
+
+    case 'LIST_ALL_NAME_CONTACTS':
+      return {
+        contacts: state.contacts.map(e => e.name),
       };
 
     case 'UPDATE_FORM':
@@ -34,7 +69,7 @@ export default (state = initialState, action) => {
         ...state,
         form: [...state.form, action.payload]
       };
-      
+
     default:
       return state
   };
