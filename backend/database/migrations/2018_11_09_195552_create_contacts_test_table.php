59f56d15a5f6e9057876c7b1b20cb5243571fc20
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-11-09 21:00:08 +0100

    create contact test table

--- backend/database/migrations/2018_11_09_195552_create_contacts_test_table.php
+++ backend/database/migrations/2018_11_09_195552_create_contacts_test_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContactsTable extends Migration
+class CreateContactsTestTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('contacts_test', function (Blueprint $table) {
             $table->increments('id');
             $table->string('user');
+            $table->string('filename');
             $table->string('name');
             $table->string('phone');
             $table->boolean('favourite');
@@ -30,6 +31,6 @@ class CreateContactsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('contacts_test');
     }
 }
