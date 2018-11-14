523c859b442214a96a7861a4bcd90d93329211c7
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-11-14 01:47:59 +0100

    update routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-// use App\Contact;
+use App\Contact;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,51 +15,47 @@ use Illuminate\Http\Request;
 |
 */
 
-// Route::post('/contacts', function (Request $request) {
-//     if ($request['sub'] !== null) {
-//         $results = DB::select('select * from contacts where user = :user', ['user' => $request['sub']]);
-//         return response()->json($results);
-//     }
-// })->middleware('jwt');
+Route::get('/contacts/{sub}', function (Request $request) {
+    if ($request['sub'] !== null) {
+        $results = DB::select('select * from contacts where user = :user', ['user' => $request['sub']]);
+        return response()->json($results);
+    }
+})->middleware('jwt');
 
 
-// Route::post('/addContacts', function(Request $request) {
+Route::post('/contacts', function(Request $request) {
 
-//     $path = $request->file('avatar')->store('images');
+    $path = $request->file('avatar')->store('images');
 
-//     $contact = new Contact;
-//     // $contact->user = $request['sub'];
-//     // $contact->name = $request['name'];
-//     // $contact->avatar = $path;
-//     // $contact->phone = $request['phone'];
-//     // $contact->favourite = $request['favourite'];
-//     // $contact->save();
-// // // Comprobar si se ha guardado y devolver el estado segun
-//     // return response()->json([
-//     //     // 'bla' => $request->file('avatar')->store('images'),
-//     //     'code' => 201,
-//     //     'status' => 'The contact is created successfully',
-//     //     'contact' => $contact,
-//     // ], 201);
-//     return response()->json(['bla' => $request->file('avatar')]);
-// })->middleware('jwt');
+    // $contact = new Contact;
+    // $contact->user = $request['sub'];
+    // $contact->name = $request['name'];
+    // $contact->avatar = $path;
+    // $contact->phone = $request['phone'];
+    // $contact->favourite = $request['favourite'];
+    // $contact->save();
+// Comprobar si se ha guardado y devolver el estado segun
+    // return response()->json([
+    //     // 'bla' => $request->file('avatar')->store('images'),
+    //     'code' => 201,
+    //     'status' => 'The contact is created successfully',
+    //     'contact' => $contact,
+    // ], 201);
+    return response()->json();
+});
 
-// Route::put('/contacts/{id}', function(Request $request, $id) {
-//     $contact = Contact::find($id);
-//     $contact->update($request->all());
-//     return response()->json([
-//         'code' => 204,
-//         'status' => 'The contact is update successfully',
-//         'contact' => DB::select('select * from contacts order by created_at desc')[0],
-//     ], 201);
-// });
+Route::put('/contacts/{id}', function(Request $request, $id) {
+    $contact = Contact::find($id);
+    $contact->update($request->all());
+    return response()->json([
+        'code' => 204,
+        'status' => 'The contact is update successfully',
+        'contact' => DB::select('select * from contacts order by created_at desc')[0],
+    ], 201);
+})->middleware('jwt');
 
-// Route::delete('/contacts/{id}', function($id) {
-//     Contact::find($id)->delete();
-// })->middleware('jwt');
+Route::delete('/contacts/{id}', function($id) {
+    Contact::find($id)->delete();
+})->middleware('jwt');
 
-// Route::post('contacts', 'ContactController')->middleware('jwt');
-// Route::post('/addContacts', 'ContactController')->name('contacts.addContacts')->middleware('jwt');
-// Route::put('contacts/{id}', 'ContactController')->middleware('jwt');
-// Route::delete('contacts/{id}', 'ContactController')->name()->middleware('jwt');
-Route::resource('contacts', 'ContactController')->middleware('jwt');
+// Route::resource('contacts', 'ContactController')->middleware('jwt');
