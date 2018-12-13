5f9d117bc991992e7c43bc165750efa67b535745
aroabarberan <aroabarberan@gmail.com>
2018-12-13 18:57:53 +0100

    fix error on first login

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { Route, Router } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
 import history from './history';
 import Auth from './Components/Auth/AuthComponent';
 import AppComponent from "./Components/AppComponent";
 import Callback from './Components/Callback/CallbackComponent';
-import MergeContactContainer from "./Containers/Contact/mergeContactContainer";
-import FavouriteContactContainer from "./Containers/Contact/favouriteContactContainer";
 import ContactContainer from "./Containers/Contact/contactContainer";
-import ShowContactGroupContainer from "./Containers/ShowContactGroupContainer";
 
 const auth = new Auth();
 
@@ -17,26 +14,28 @@ const handleAuthentication = ({ location }) => {
   }
 }
 
-const redirectToLoginIfNecessary = ({ location }) => {
-  if (!location.pathname.includes('/callback') && !auth.isAuthenticated()) {
-    auth.login();
-  }
-}
-
 export const makeMainRoutes = () => {
   return (
     <Router history={history}>
       <div>
-        <Route render={redirectToLoginIfNecessary} />
-        <Route path="/" render={(props) => <AppComponent auth={auth} {...props} />} />
-        <Route path="/contacts" render={(props) => <ContactContainer auth={auth} {...props} />} />
-        <Route path="/merge" render={(props) => <MergeContactContainer auth={auth} {...props} />} />
-        <Route path="/favourite" render={(props) => <FavouriteContactContainer auth={auth} {...props} />} />
-        <Route path="/group/:nameGroup" render={(props) => <ShowContactGroupContainer auth={auth} {...props} />} />
-        <Route path="/callback" render={(props) => {
-          handleAuthentication(props)
-          return <Callback {...props} />
-        }} />
+        <Switch>
+          <Route exact path='/callback'>
+            <Route path="/callback" render={(props) => {
+              handleAuthentication(props)
+              return <Callback {...props} />
+            }} />
+          </Route>
+          <Route render={() => (
+            <Fragment>
+              <Route render={() => { if (!auth.isAuthenticated()) auth.login(); }} />
+              <Route path="/" render={(props) => <AppComponent auth={auth} {...props} />} />
+              <Route path="/contacts" render={(props) => <ContactContainer auth={auth} {...props} />} />
+              <Route path="/merge" render={(props) => <ContactContainer auth={auth} {...props} />} />
+              <Route path="/favourite" render={(props) => <ContactContainer auth={auth} {...props} />} />
+              <Route path="/group/:nameGroup" render={(props) => <ContactContainer auth={auth} {...props} />} />
+            </Fragment>
+          )}/>
+        </Switch>
       </div>
     </Router>
   )
