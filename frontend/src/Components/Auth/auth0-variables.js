39131346ec63d9f2c86a43aea2231223991e1337
aroabarberan <aroabarberan@gmail.com>
2018-12-14 06:47:09 +0100

    start adding new auth0 config to frontend and add different enviroments

--- frontend/src/Components/Auth/auth0-variables.js
+++ frontend/src/Components/Auth/auth0-variables.js
@@ -1,10 +1,25 @@
+// export const AUTH_CONFIG = {
+//   domain: 'bookcontacts.auth0.com',
+//   clientId: 'C1r5FLArX05HulvxLJxMqwRkA8be5sCv',
+//   clientSecret: 'BTbGssuKoQXujgZHGalMEj8e-jlBwh6i833CUSvjSRN4keM7dXtT7GBoEafS0HYF',
+//   callbackUrl: 'http://localhost:3000/callback',
+//   // callbackUrl: 'https://contactboard.tk/callback',
+//   responseType: 'token id_token',
+//   audience: 'https://apibookcontacts',
+//   scope: 'openid profile'
+// }
+
 export const AUTH_CONFIG = {
-  domain: 'bookcontacts.auth0.com',
-  clientId: 'C1r5FLArX05HulvxLJxMqwRkA8be5sCv',
-  clientSecret: 'BTbGssuKoQXujgZHGalMEj8e-jlBwh6i833CUSvjSRN4keM7dXtT7GBoEafS0HYF',
-  callbackUrl: 'http://localhost:3000/callback',
-  // callbackUrl: 'https://contactboard.tk/callback',
+  domain: 'contactboard.eu.auth0.com',
+  clientId: 'm8iuTfWx5BYpgzJdU51RZL0xWTuUvJHp',
+  clientSecret: '4X567sowOeI-OH71ulRHKYJYauI6ryGnmGNmB0E64ovPx4aPw3Y2uqC4y-qVph7V',
+  callbackUrl: 'https://contacboard.tk/callback',
   responseType: 'token id_token',
-  audience: 'https://apibookcontacts',
+  audience: 'https://api.contactboard.tk/',
   scope: 'openid profile'
 }
+
+if (process.env && process.env.NODE_ENV === 'development') {
+  AUTH_CONFIG.callbackUrl = 'http://localhost:3000/callback'
+}
+console.log(AUTH_CONFIG)
--- frontend/src/querys.js
+++ frontend/src/querys.js
@@ -1,10 +1,13 @@
 export const QUERIES = {
-  // contact: 'https://api.contactboard.tk/api/contacts/',
-  // group: 'https://api.contactboard.tk/api/groups/',
-  // contactgroup: 'https://api.contactboard.tk/api/contactgroup/',
-  // phone: 'https://api.contactboard.tk/api/phones/',
-  contact: 'http://localhost:3010/api/contacts/',
-  group: 'http://localhost:3010/api/groups/',
-  contactgroup: 'http://localhost:3010/api/contactgroup/',
-  phone: 'http://localhost:3010/api/phones/',
+  contact: 'https://api.contactboard.tk/contacts/',
+  group: 'https://api.contactboard.tk/groups/',
+  contactgroup: 'https://api.contactboard.tk/contactgroup/',
+  phone: 'https://api.contactboard.tk/phones/',
+}
+
+if (process.env && process.env.NODE_ENV === 'development') {
+  QUERIES.contact = 'http://localhost:3010/contacts/'
+  QUERIES.group = 'http://localhost:3010/groups/'
+  QUERIES.contactgroup = 'http://localhost:3010/contactgroup/'
+  QUERIES.phone = 'http://localhost:3010/phones/'
 }
